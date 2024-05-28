ace5b52008faf02121edfa65c9198e78c3b9553f
AdelMysterio <storliniadelmo@gmail.com>
2024-05-28 12:55:42 +0200

    Svolto bonus 1

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        message: 'Eh VUElevi!',
+        fonteImg: 'https://libero-gaming-r2.kingesport.com/Zeb_Volevi_ff33e2ef9f.jpg'
       }
     }
   }).mount('#app')
